fc15106fa163047fdfac3c52d038a6f47c0f601c
Mark Emeis <mark.emeis@weave.works>
2021-06-07 17:06:50 -0600

    Fix branch and dir name in the edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -66,13 +66,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/weaveworks/weave-gitops-docs/edit/master/website/',
+            'https://github.com/weaveworks/weave-gitops-docs/edit/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/weaveworks/weave-gitops-docs/edit/master/website/blog/',
+            'https://github.com/weaveworks/weave-gitops-docs/edit/main/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
