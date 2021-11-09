e6b12ffa9ea4197d387ec02eaa4b9a386e29f4ca
Jordan Pellizzari <jordan@weave.works>
2021-11-09 10:12:20 -0800

    Format doc config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,70 +1,78 @@
-const versions = require('./versions.json');
+const versions = require("./versions.json");
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
+  title: "Weave GitOps",
+  tagline: "Weave GitOps Documentation",
+  url: "https://docs.gitops.weave.works/",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon_150px.png",
+  organizationName: "weaveworks", // Usually your GitHub org/user name.
+  projectName: "weave-gitops-docs", // Usually your repo name.
   plugins: ["@docusaurus/plugin-google-gtag"],
   themeConfig: {
     navbar: {
-      title: 'Weave GitOps',
+      title: "Weave GitOps",
       logo: {
-        alt: 'Weave GitOps Logo',
-        src: 'img/weave-logo.png',
+        alt: "Weave GitOps Logo",
+        src: "img/weave-logo.png",
       },
       items: [
         {
-          type: 'doc',
-          docId: 'intro',
-          position: 'left',
-          label: 'Introduction',
+          type: "doc",
+          docId: "intro",
+          position: "left",
+          label: "Introduction",
         },
         {
-          type: 'doc',
-          docId: 'installation',
-          position: 'left',
-          label: 'Installation',
+          type: "doc",
+          docId: "installation",
+          position: "left",
+          label: "Installation",
         },
         {
-          type: 'doc',
-          docId: 'getting-started',
-          position: 'left',
-          label: 'Getting Started',
+          type: "doc",
+          docId: "getting-started",
+          position: "left",
+          label: "Getting Started",
         },
         {
-            type: 'doc',
-            docId: 'aws-marketplace',
-            position: 'left',
-            label: 'AWS Marketplace',
+          type: "doc",
+          docId: "aws-marketplace",
+          position: "left",
+          label: "AWS Marketplace",
         },
         {
-          type: 'docsVersionDropdown',
-          position: 'right',
+          type: "docsVersionDropdown",
+          position: "right",
           dropdownActiveClassDisabled: true,
         },
         {
-          href: 'https://github.com/weaveworks/weave-gitops',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/weaveworks/weave-gitops",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Support',
+          title: "Support",
           items: [
             {
-              label: 'Contact Us',
-              href: 'mailto:support@weave.works',
+              label: "Contact Us",
+              href: "mailto:support@weave.works",
             },
           ],
-         },
+        },
       ],
       copyright: `Copyright © ${new Date().getFullYear()} Weaveworks`,
     },
     algolia: {
-      
       apiKey: process.env.ALGOLIA_API_KEY,
-      indexName: 'weave',
+      indexName: "weave",
       // Needed to handle the different versions of docs
       contextualSearch: true,
 
@@ -72,7 +80,6 @@ module.exports = {
       // searchParameters: {
       //   facetFilters: ['type:content']
       // },
-      
     },
     gtag: {
       // You can also use your "G-" Measurement ID here.
@@ -83,16 +90,16 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/weaveworks/weave-gitops-docs/edit/main/',
+          editUrl: "https://github.com/weaveworks/weave-gitops-docs/edit/main/",
           lastVersion: versions[0],
           versions: {
             current: {
-              label: 'main',
+              label: "main",
             },
           },
         },
@@ -100,10 +107,10 @@ module.exports = {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/weaveworks/weave-gitops-docs/edit/main/blog/',
+            "https://github.com/weaveworks/weave-gitops-docs/edit/main/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
